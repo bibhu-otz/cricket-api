6aa77562ac69d37cf0a1acbc4fdcee1d74810359
BIBHU RANJAN MOHANTY <78833163+bibhu-otz@users.noreply.github.com>
2025-09-08 12:52:37 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from functools import lru_cache
 class Settings(BaseSettings):
   db_user: str = "postgres"
-  db_password: str = "csmpl@123"
+  db_password: str = ""
   db_host: str = "localhost"
   db_port: str = "5432"
   db_name: str = "cricket_db"
